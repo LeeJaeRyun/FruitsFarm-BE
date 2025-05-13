b16d571c8cb960ae5094851cd6f3ce3352040ff5
LeeJaeRyun <pinixs3@naver.com>
2025-05-13 22:20:00 +0900

    [CHORE]: 자바 버전 변경 & addCorsMappings 추가

--- src/main/java/jnu_ddobuk/fruitsfarm_BE/web/config/WebConfig.java
+++ src/main/java/jnu_ddobuk/fruitsfarm_BE/web/config/WebConfig.java
@@ -2,12 +2,24 @@ package jnu_ddobuk.fruitsfarm_BE.web.config;
 
 import jnu_ddobuk.fruitsfarm_BE.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://fruitsfarm.com",
+                        "http://fruitsfarm.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3000); // pre-flight 요청을 캐시할 시간 (초 단위)
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
